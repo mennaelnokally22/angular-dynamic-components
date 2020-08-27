aaf8decb42354ec72c94bb7ce2da886f2d54551f
Ibrahim <mennaallaah.ibrahim@vodafone.com>
2020-08-27 16:13:36 +0200

    ✨ NEW FEATURE: Test component added to load into dialog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DialogModule } from './dialog/dialog.module';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent  ],
+    AppComponent,
+    TestComponent  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
